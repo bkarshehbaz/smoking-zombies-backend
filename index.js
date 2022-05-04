e415f57f869de91947f38749a3abcb90e2605a49
tomngdevv <tomng2794@gmail.com>
2022-05-05 01:50:27 +0800

    adding cors

--- index.js
+++ index.js
@@ -16,8 +16,20 @@ const app = express();
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(helmet());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
 app.use(cors());
+app.use(helmet());
 
 if (process.env.NODE_ENV !== 'production') {
   // app.use(morgan('dev'));
