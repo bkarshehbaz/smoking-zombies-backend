e2198e5661458072d525f13297e3c7f6f901e844
tomngdevv <tomng2794@gmail.com>
2022-05-07 00:48:06 +0800

    deleted update shop and delete shop bullshit

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -5,15 +5,10 @@ const { ValidateJWT } = require('../services/utils');
 const {
   getAllShopsData,
   createOneShop,
-  updateShopData,
 } = require('../controllers/shop.controller');
 
 router.get('/getAllShopsData', ValidateJWT, getAllShopsData);
 
 router.post('/createOneShop', ValidateJWT, createOneShop);
-
-router.get('/updateOneShop', ValidateJWT, updateShopData);
-
-router.get('/deleteOneShop', ValidateJWT, getAllShopsData);
 
 module.exports = router;
